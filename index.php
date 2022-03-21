e5045b66ddbcd85ebf3c7a04424e3493075ae225
Joran Schrievers <99062987@mydavinci.nl>
2022-03-21 16:12:18 +0100

    Made a begin

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+class Pokemon{
+    private static $count=0;
+    private $name;
+    private $energyType;
+    private $maxHP;
+    private $attack = [];
+    private $health;
+    private $resistanceAmount;
+    private $weaknessMultiplier;
+    private $weaknessEnergyType;
+}
+
+public function _construct($name, $energyType,$maxHP,$attack,$health,$resistanceAmout,$weaknessMultiplier,$weaknessEnergyType){
+
+}
+?>
